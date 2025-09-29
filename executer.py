ced287f6d99296cc6a6ba18403d27f9f937e1b81
Wyatt McDonald <WhyMcD@gmail.com>
2025-09-29 10:38:35 -0400

    Fixed position sizing

--- executer.py
+++ executer.py
@@ -5,6 +5,11 @@ from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest, LimitOrderRequest
 from risk import place_capped_buy
 
+
+
+
+
+
 def execute_intent(intent: Dict[str, Any],
                    equity_trader: TradingClient,
                    options_trader) -> Any:
